92e29453e15ed4bf93b79d25693d1eb1f363ff7a
Ashley Rich <hello@ashleyrich.com>
2019-03-11 13:15:03 +0000

    Actually delete files!

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit2a390f847c54e3cba8df04750fba0b1d::getLoader();
+return ComposerAutoloaderInitda430d117603a53ee23aa0f55dcbf6a8::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,11 +4,27 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit2a390f847c54e3cba8df04750fba0b1d
+class ComposerStaticInitda430d117603a53ee23aa0f55dcbf6a8
 {
+    public static $prefixLengthsPsr4 = array (
+        'D' => 
+        array (
+            'DeliciousBrains\\SpinupWp\\' => 25,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'DeliciousBrains\\SpinupWp\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitda430d117603a53ee23aa0f55dcbf6a8::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitda430d117603a53ee23aa0f55dcbf6a8::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }

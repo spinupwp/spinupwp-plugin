db4190bc395eff941ed2e8e94c9d95e58c76a974
Gilbert Pellegrom <gilbert@pellegrom.me>
2019-05-09 10:15:54 +0100

    Refactor dismissing notices

--- assets/js/dismiss-notice.js
+++ assets/js/dismiss-notice.js
@@ -1,10 +1,11 @@
 jQuery( document ).ready( function( $ ) {
 	$( '.spinupwp.notice.is-dismissible' ).on( 'click', '.notice-dismiss', function ( e ) {
 		e.preventDefault();
-		
+
 		$.post( window.ajaxurl, {
 			action: 'spinupwp_dismiss_notice',
 			nonce: $( this ).parent().data( 'nonce' ),
+			notice: $( this ).parent().data( 'notice' ),
 		} );
 	} );
 } );

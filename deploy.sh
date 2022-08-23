ca1f4d0522a8172b8d245ad4f84a217c7a94a45c
Ashley Rich <hello@ashleyrich.com>
2022-08-23 22:34:25 +0100

    Don't upload composer.lock to WP.org

--- deploy.sh
+++ deploy.sh
@@ -165,7 +165,8 @@ Thumbs.db
 .github/*
 .git
 .gitattributes
-.gitignore" "$SVNPATH/trunk/"
+.gitignore
+composer.lock" "$SVNPATH/trunk/"
 
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix="$SVNPATH/trunk/"

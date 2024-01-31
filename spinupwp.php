77530003154782c44d53ecbe1f5e2bdd3777572b
James Clark <james@weno.net>
2024-01-31 11:59:13 -0500

    bump version number

--- spinupwp.php
+++ spinupwp.php
@@ -4,7 +4,7 @@ Plugin Name:  SpinupWP
 Plugin URI:   https://spinupwp.com
 Description:  SpinupWP helper plugin.
 Author:       SpinupWP
-Version:      1.5.1
+Version:      1.6
 Network:      True
 Text Domain:  spinupwp
 Requires PHP: 7.1
